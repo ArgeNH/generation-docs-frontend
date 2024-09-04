36744f6d58cd60892658fd1d7136f9cbdfb1e6d9
ArgeNH <arge.nino@uptc.edu.co>
2024-09-03 21:48:29 -0500

    initial commit gen-docs frontend 🚀

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="content">
+      <h1>Generation Docs</h1>
+      <p>Start building amazing things! 🚀</p>
+    </div>
+  );
+};
+
+export default App;
